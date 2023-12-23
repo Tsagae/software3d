3d4ace1e66df604b4a7b8b470d755a97e136487a
Matteo Zagheno <matteo2309@hotmail.com>
2023-12-23 21:21:54 +0100

    added imagebuffer benchmarks

--- pkg/graphics/imagebuffer_test.go
+++ pkg/graphics/imagebuffer_test.go
@@ -2,6 +2,7 @@ package graphics
 
 import (
 	"fmt"
+	"image/color"
 	"testing"
 	time2 "time"
 )
@@ -13,5 +14,53 @@ func BenchmarkImageBuffer_Clear(b *testing.B) {
 	time := time2.Now()
 	imageBuffer.Clear()
 	fmt.Printf("clearing image buffer: %v\n", time2.Now().Sub(time))
+	fmt.Println("----------------Benchmark end----------------")
+}
+
+func BenchmarkImageBuffer_Get(b *testing.B) {
+	imageBuffer := NewImageBuffer(800, 600)
+	fmt.Println("---------------Benchmark start---------------")
+	b.ResetTimer()
+	time := time2.Now()
+	for i := 0; i < b.N; i++ {
+		imageBuffer.Get(0, 0)
+	}
+	fmt.Printf("accessing %v pixels: %v\n", b.N, time2.Now().Sub(time))
+	fmt.Println("----------------Benchmark end----------------")
+}
+
+func BenchmarkImageBuffer_Set(b *testing.B) {
+	imageBuffer := NewImageBuffer(800, 600)
+	fmt.Println("---------------Benchmark start---------------")
+	b.ResetTimer()
+	time := time2.Now()
+	for i := 0; i < b.N; i++ {
+		imageBuffer.Set(0, 0, color.RGBA{
+			R: 255,
+			G: 255,
+			B: 255,
+			A: 255,
+		})
+	}
+	fmt.Printf("setting %v pixels: %v\n", b.N, time2.Now().Sub(time))
+	fmt.Println("----------------Benchmark end----------------")
+}
+
+func BenchmarkImageBufferFillImage(b *testing.B) {
+	imageBuffer := NewImageBuffer(800, 600)
+	fmt.Println("---------------Benchmark start---------------")
+	b.ResetTimer()
+	time := time2.Now()
+	for y := 0; y <= 600; y++ {
+		for x := 0; x <= 800; x++ {
+			imageBuffer.Set(x, y, color.RGBA{
+				R: 255,
+				G: 255,
+				B: 255,
+				A: 255,
+			})
+		}
+	}
+	fmt.Printf("filling image buffer: %v\n", time2.Now().Sub(time))
 	fmt.Println("----------------Benchmark end----------------")
 }
