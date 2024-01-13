9cdf79a9cd15e5d33a82e93f64bab83c64cb4773
Matteo Zagheno <matteo2309@hotmail.com>
2024-01-13 14:00:23 +0100

    added coplanar vectors

--- pkg/basics/plane.go
+++ pkg/basics/plane.go
@@ -1,14 +1,27 @@
 package basics
 
 type Plane struct {
-	Point  Vector3
-	Normal Vector3
+	point     Vector3
+	normal    Vector3
+	coplanarA Vector3
+	coplanarB Vector3
 }
 
 func NewPlaneFromPointNormal(point *Vector3, normal *Vector3) Plane {
+	pNormal := normal.Normalized()
+	temp := pNormal
+	temp.X += 4129
+	temp.Y += 4133
+	temp.Z += 4139
+	temp.ThisNormalize()
+
+	coplanarA := pNormal.Cross(&temp).Normalized()
+	coplanarB := pNormal.Cross(&coplanarA).Normalized()
 	return Plane{
-		Point:  *point,
-		Normal: normal.Normalized(),
+		point:     *point,
+		normal:    pNormal,
+		coplanarA: coplanarA,
+		coplanarB: coplanarB,
 	}
 }
 
@@ -17,13 +30,25 @@ func NewPlaneFromPointNormal(point *Vector3, normal *Vector3) Plane {
 // 1 if it's in front,
 // 2 if it's on the plane
 func (p *Plane) TestPoint(point *Vector3) uint8 {
-	dist := point.Sub(&p.Point)
+	dist := point.Sub(&p.point)
 	dist.ThisNormalize()
-	if dist.Dot(&p.Normal).IsZero() {
+	if dist.Dot(&p.normal).IsZero() {
 		return 2
 	}
-	if dist.Dot(&p.Normal) > 0 {
+	if dist.Dot(&p.normal) > 0 {
 		return 1
 	}
 	return 0
+}
+
+func (p *Plane) CoplanarVectors() (Vector3, Vector3) {
+	return p.coplanarA, p.coplanarB
+}
+
+func (p *Plane) Point() Vector3 {
+	return p.point
+}
+
+func (p *Plane) Normal() Vector3 {
+	return p.normal
 }
--- pkg/basics/plane_test.go
+++ pkg/basics/plane_test.go
@@ -1,7 +1,9 @@
 package basics
 
-import "github.com/stretchr/testify/assert"
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestTestPoint(t *testing.T) {
 	planeOrigin := NewVector3(0, 0, 0)
@@ -16,4 +18,19 @@ func TestTestPoint(t *testing.T) {
 
 	pointOnPlane := NewVector3(5, 6, 0)
 	assert.Equal(t, byte(2), plane.TestPoint(&pointOnPlane))
+}
+
+func TestGetCoplanarVectors(t *testing.T) {
+	planeOrigin := NewVector3(0, 0, 0)
+	planeNormal := NewVector3(0, 0, 1)
+	plane := NewPlaneFromPointNormal(&planeOrigin, &planeNormal)
+
+	v1, v2 := plane.CoplanarVectors()
+	pNormal := plane.Normal()
+	assert.Truef(t, v1.Dot(&pNormal).IsZero(), "Dot between %v and %v has to be zero for them to be coplanar", v1, pNormal)
+	assert.True(t, v1.Length().Equals(1))
+
+	assert.Truef(t, v2.Dot(&pNormal).IsZero(), "Dot between %v and %v has to be zero for them to be coplanar", v2, pNormal)
+	assert.True(t, v2.Length().Equals(1))
+
 }
