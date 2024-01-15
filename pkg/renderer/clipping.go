1b3f7ade4bd6c7f3f1d172a809fb3f953b0c2495
Matteo Zagheno <matteo2309@hotmail.com>
2024-01-15 10:24:12 +0100

    renamed ClipSegment to FindIntersectionPoint

--- pkg/renderer/clipping.go
+++ pkg/renderer/clipping.go
@@ -4,33 +4,33 @@ import (
 	"github.com/tsagae/software3d/pkg/basics"
 )
 
-// ClipSegment return the point where the plane intersects the segment and true if there is one, false if it's not found, they're the same point or one of them is coplanar
-func ClipSegment(p0, p1 *basics.Vector3, plane *basics.Plane) (basics.Vector3, bool) {
+// FindIntersectionPoint return the point where the plane intersects the segment and true if there is one or if one of them is coplanar, false if it's not found, they're the same point. Returns also the 2 tests against the plane for the 2 points
+func FindIntersectionPoint(p0, p1 *basics.Vector3, plane *basics.Plane) (basics.Vector3, bool, uint8, uint8) {
 	test0 := plane.TestPoint(p0)
 	test1 := plane.TestPoint(p1)
 
 	if test0 == 2 {
-		return basics.Vector3{}, false
+		return *p0, true, test0, test1
 	}
 	if test1 == 2 {
-		return basics.Vector3{}, false
+		return *p1, true, test0, test1
 	}
 	if test0 == test1 {
-		return basics.Vector3{}, false
+		return basics.Vector3{}, false, test0, test1
 	}
 
 	lineN := p1.Sub(p0)
-	planeP := plane.Point()
-	planeN := plane.Normal()
+	planeP := plane.Point
+	planeN := plane.Normal
 
 	p1MinP2 := planeP.Sub(p0)
 	d := lineN.Dot(&planeN)
 	if d.IsZero() {
-		return basics.Vector3{}, false
+		return basics.Vector3{}, false, test0, test1
 	}
 	k := p1MinP2.Dot(&planeN) / lineN.Dot(&planeN)
 
 	nK := lineN.Mul(k)
 	intersection := p0.Add(&nK)
-	return intersection, true
+	return intersection, true, test0, test1
 }
--- pkg/renderer/clipping_test.go
+++ pkg/renderer/clipping_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestClipSegment(t *testing.T) {
+func TestFindIntersectionPoint(t *testing.T) {
 	p0 := basics.NewVector3(-1, 0, +1)
 	p1 := basics.NewVector3(+1, 0, +1)
 	planePoint := basics.NewVector3(0, 0, 0)
@@ -14,19 +14,26 @@ func TestClipSegment(t *testing.T) {
 	plane := basics.NewPlaneFromPointNormal(&planePoint, &planeNormal)
 
 	//plane intersects segment
-	clippingPoint, isClipped := ClipSegment(&p0, &p1, &plane)
+	clippingPoint, isClipped, _, _ := FindIntersectionPoint(&p0, &p1, &plane)
 
 	assert.True(t, isClipped)
 
 	expected := basics.NewVector3(0, 0, 1)
 	assert.True(t, clippingPoint.Equals(&expected))
 
-	//plane doesen't intersect segment
+	//plane intersects segment
+	clippingPoint, isClipped, _, _ = FindIntersectionPoint(&p1, &p0, &plane)
+
+	assert.True(t, isClipped)
+
+	assert.True(t, clippingPoint.Equals(&expected))
+
+	//plane doesn't intersect segment
 	planePoint2 := basics.NewVector3(0, 1, 0)
 	planeNormal2 := basics.NewVector3(0, 1, 0)
 	plane2 := basics.NewPlaneFromPointNormal(&planePoint2, &planeNormal2)
 
-	_, isClipped = ClipSegment(&p0, &p1, &plane2)
+	_, isClipped, _, _ = FindIntersectionPoint(&p0, &p1, &plane2)
 
 	assert.False(t, isClipped)
 
@@ -35,7 +42,7 @@ func TestClipSegment(t *testing.T) {
 	p1 = basics.NewVector3(-1, 0, +1)
 
 	//no intersection if the segment is a point
-	clippingPoint, isClipped = ClipSegment(&p0, &p1, &plane)
+	clippingPoint, isClipped, _, _ = FindIntersectionPoint(&p0, &p1, &plane)
 	assert.False(t, isClipped)
 
 	//line intersects the plane but segment doesn't
@@ -43,10 +50,6 @@ func TestClipSegment(t *testing.T) {
 	p1 = basics.NewVector3(2, 0, +1)
 
 	//plane does not segment
-	clippingPoint, isClipped = ClipSegment(&p0, &p1, &plane)
+	clippingPoint, isClipped, _, _ = FindIntersectionPoint(&p0, &p1, &plane)
 	assert.False(t, isClipped)
-}
-
-func TestClipTriangle(t *testing.T) {
-
 }
